905132ab387201487202b95704c8829ccbea5c41
Matthew Glasser <matthewsglasser@gmail.com>
2023-05-12 18:22:23 -0400

    debugging

--- scheduler.js
+++ scheduler.js
@@ -1,23 +1,43 @@
-function createSchedule(events) {
-  var schedule = [];
+function blockSchedule(events) {
+  events.sort((a, b) => a.length - b.length);
+  const schedule = [];
 
-  events.sort((a,b) => a.endTime - b.endTime);
-  var end = 0;
-  for (var i = 0; i < events.length; i++) {
-    let event = events[i];
-    if(event.startTime >= end) {
-      schedule.push(event);
-      end = event.endTime;
+  for(let i=0; i<events.length; i++) {
+    var event = events[i];
+    console.log("Event:", event[0]);
+    
+    for(let q=1; q < event.length; q++) {
+      var timeSlot = event[q];
+      console.log("timeSlot", timeSlot);
+      if(!conflicts(schedule, timeSlot)) {
+        let toAdd = [event[0], timeSlot];
+        console.log("to add",toAdd);
+
+        schedule.push(toAdd); 
+        console.log("Added", [toAdd], "to schedule");
+      }
     }
   }
+  schedule.push("asdhkdhakhsdflksfdh");
   return schedule;
 }
 
-const events = [
-  {name: 'Event A', startTime: 10, endTime: 18},
-  {name: 'Event B', startTime: 11, endTime: 12},
-  {name: 'Event C', startTime: 13, endTime: 14},
-  {name: 'Event D', startTime: 7, endTime: 11},
-];
+function conflicts(events, period){
+  for(let i=0; i<events.length; i++) {
+    if(events[i][1] = period) {
+      console.log("Conflict between", events[i],"and", period);
+      return true;
+    }
+  }
+  return false;
+}
 
-console.log(createSchedule(events));
+
+const eventsToSchedule = [
+  ["Event A", 1, 2, 5],
+  ["Event B", 2, 4],
+  ["Event C", 5],
+  ["Event D", 3, 4, 7, 9],
+]
+
+console.log(blockSchedule(eventsToSchedule));
