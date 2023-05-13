c5bfa69559f627593b563978b01a55bae2bc7274
Matthew Glasser <matthewsglasser@gmail.com>
2023-05-13 10:35:55 -0400

    now works

--- scheduler.js
+++ scheduler.js
@@ -4,34 +4,32 @@ function blockSchedule(events) {
 
   for(let i=0; i<events.length; i++) {
     var event = events[i];
-    console.log("Event:", event[0]);
+    // console.log("Event:", event[0]);
     
     for(let q=1; q < event.length; q++) {
       var timeSlot = event[q];
-      console.log("timeSlot", timeSlot);
+      // console.log("timeSlot", timeSlot);
       if(!conflicts(schedule, timeSlot)) {
         let toAdd = [event[0], timeSlot];
-        console.log("to add",toAdd);
 
         schedule.push(toAdd); 
-        console.log("Added", [toAdd], "to schedule");
+        // console.log("Added", [toAdd], "to schedule");
+        // console.log("Current Schedule:", schedule)
       }
     }
   }
-  schedule.push("asdhkdhakhsdflksfdh");
   return schedule;
 }
 
 function conflicts(events, period){
   for(let i=0; i<events.length; i++) {
-    if(events[i][1] = period) {
-      console.log("Conflict between", events[i],"and", period);
+    if(events[i][1] == period) {
+      // console.log("Conflict between", events[i],"and", period);
       return true;
     }
   }
   return false;
 }
-
 
 const eventsToSchedule = [
   ["Event A", 1, 2, 5],
